1e32c062f394f6de329fbf90dcc7ca5fb3eb9534
Nkosana Ncube <nkosanazazu@gmail.com>
2026-09-20 21:59:17 +0900

    Day 1: Learn Python variables and user input

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+age = input("How old are you?")
+country = input("What country are you from?")
+
+
+print("Hello,",name,age,country)
+print("Welcome to your Python backend journey!")
