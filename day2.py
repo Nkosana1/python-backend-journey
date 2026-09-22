c5e06fee423fd64233d568da9bc757cf84d00766
Nkosana Ncube <nkosanazazu@gmail.com>
2026-09-22 19:36:28 +0900

    Day 2 - 9/21/26: Python data types and type conversion

---
+++ day2.py
@@ -0,0 +1,17 @@
+name = "Nkosana"
+age = 39
+height = 1.74
+learning_python = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(learning_python))
+
+age = input("How old are you?")
+print(type(age))
+
+age = int(input("How old are you?"))
+print(type(age))
+next_age = age + 1
+print("Next year you will be",next_age)
