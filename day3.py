34f05f715b931f70c916f8cd31da62586c04da26
Nkosana Ncube <nkosanazazu@gmail.com>
2026-09-22 21:24:08 +0900

    Day 3: Build Python arithmetic calculator

---
+++ day3.py
@@ -0,0 +1,17 @@
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+
+total = number1 + number2
+
+print(f"The total is {total}")
+
+
+number1 = int(input("Enter the first number:"))
+number2 = int(input("Enter the second number:"))
+
+total = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+division = number1 / number2
+
+print(f"The total is {total}. The difference is {difference}.The product is {product}. The division result is {division}.")
